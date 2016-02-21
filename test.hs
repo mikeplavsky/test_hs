6722c778dabcdc1cfe9d9526fee1cbdd646c93c9
mikeplavsky <mike.plavsky@quest.com>
2016-02-21 09:58:38 +0000

    fib

---
+++ test.hs
@@ -0,0 +1,36 @@
+import System.Environment
+
+fib_s = 1 : 1 : [a + b | (a,b) <- zip fib_s $ tail fib_s]
+
+getH = do
+    x <- getEnvironment
+    return [b | (a,b) <- x, a == "HOSTNAME"]
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "Wow!"
+lucky x = "No!"
+
+fib 0 = 1
+fib 1 = 1
+fib n = fib(n-1) + fib(n-2)
+
+fib' x 
+    | x == zero = one
+    | x == one = one
+    | otherwise = fib'(x-one) + fib'(x-two)
+    where zero = 0
+          one = 1
+          two = 2 
+
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+qs [] = []
+qs (x:xs) = 
+    let smS = qs [a | a <- xs, a <= x]
+        bS = qs [a | a <- xs, a > x]
+    in smS ++ [x] ++ bS    
