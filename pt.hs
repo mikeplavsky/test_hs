9b586a7164bb952b534f0941475e8ee18ab36178
mikeplavsky <mike.plavsky@quest.com>
2016-02-23 10:20:55 +0000

    getting JSON

--- pt.hs
+++ pt.hs
@@ -1,9 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wreq
 import Control.Lens
+import Data.Aeson (Array)
 
 projects = "https://www.pivotaltracker.com/services/v5/projects"
+type Resp = Response (Array)
 
+get_projects1 tk =
+
+    let opts = defaults & header "X-TrackerToken" .~ [tk]
+    in 
+       let d = getWith opts projects
+           d1 = asJSON =<< d :: IO Resp
+       in d1
+        
 get_projects tk =
 
     let opts = defaults & header "X-TrackerToken" .~ [tk]
