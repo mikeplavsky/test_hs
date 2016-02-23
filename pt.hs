31880d44f314e2e9a7bf110a201efbb1d427734b
Ubuntu <ubuntu@ip-10-95-185-165.ec2.internal>
2016-02-23 09:42:24 +0000

    with token

--- pt.hs
+++ pt.hs
@@ -1,14 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Network.Wreq
 import Control.Lens
 
 projects = "https://www.pivotaltracker.com/services/v5/projects"
 
-get_projects tk = do
-    r <- get projects
-    return  r
+get_projects tk =
 
-token = "ABCD"
+    let opts = defaults & header "X-TrackerToken" .~ [tk]
+    in do getWith opts projects
 
-main = do 
-    r <-(get_projects token)
-    putStrLn $ show $ r ^. responseStatus
+
