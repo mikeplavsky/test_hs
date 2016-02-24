a27bf6674493ee7d615a75cd24f488ebd280faf1
mikeplavsky <mike.plavsky@quest.com>
2016-02-24 07:53:09 +0000

    implementing monad

--- md.hs
+++ md.hs
@@ -3,5 +3,12 @@ data Result a = Data a | Error (a, String) deriving Show
 getIt (-1) = Error(-1, "too negative")
 getIt a = Data a
 
+instance Functor Result where
+    fmap f (Data a) = Data (f a) 
+
+instance Applicative Result where
+    pure = Data
+    (<*>) (Data f) (Data a) = Data (f a)
+
 instance Monad Result where
     (>>=) (Data a) f = f a
