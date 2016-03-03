9518094e671886d6a904e3fd4766467eed9615d6
mikeplavsky <mike.plavsky@quest.com>
2016-03-03 12:43:55 +0300

    even lazier

--- upper.hs
+++ upper.hs
@@ -1,2 +1,15 @@
 import Data.Char (toUpper)
-main = interact (map toUpper)
+-- main = interact (map toUpper . (++) "Your data:\n\n")
+main = interact ((++) "Your data:\n\n" . (map toUpper))
+
+z = 'a':[x | x <- z]
+f = (++) "Yours: " . map toUpper
+
+check = take 10 (f z)
+
+z1 = "aaa" : [x | x <- z1]
+x = unlines z1
+
+g = filter (elem 'a') . lines
+g1 = (take 10 . g ) x
+g2 = (g . take 10) x
