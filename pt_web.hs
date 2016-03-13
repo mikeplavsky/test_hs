98b6ba433407d0f682f9fc8ccd45487aa94e81b9
mikeplavsky <mike.plavsky@quest.com>
2016-03-13 11:53:41 +0000

    recursion but does not work yet

--- pt_web.hs
+++ pt_web.hs
@@ -6,13 +6,14 @@ import Control.Lens
 import Data.Aeson (Array, FromJSON, ToJSON, decode)
 import Data.Text
 import GHC.Generics
+import Data.Maybe
 
 pt_url = "https://www.pivotaltracker.com/services/v5/projects"
 pt_stories = pt_url ++ "/1367594/iterations?offset=" 
 
 data Story = Story {
       name :: Text
-  } deriving (Show, Generic)
+  } deriving (Eq, Show, Generic)
 
 instance FromJSON Story
 
@@ -20,14 +21,27 @@ data Iteration = Iteration {
       start :: Text,
       finish :: Text,
       stories :: [Story]
-  } deriving (Show, Generic)
+  } deriving (Eq, Show, Generic)
 
 instance FromJSON Iteration
 
 get_raw tk offset =
     let opts = defaults & header "X-TrackerToken" .~ [tk]
-    in do getWith opts $ pt_stories ++ offset
+    in do getWith opts $ pt_stories ++ (show offset)
 
 get_iterations s = 
     let d = s ^. responseBody
     in decode d :: Maybe [Iteration]
+
+get_all tk offset its more
+
+    | not more = its
+    | more = do 
+        
+        s <- get_raw tk offset 
+        let i = fromJust $ get_iterations s 
+
+        if i == []
+          then get_all tk offset its False
+          else get_all tk (offset + 10) its True
+
