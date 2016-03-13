6ac707ae8f294d273fbdb802f381349791a49790
mikeplavsky <mike.plavsky@quest.com>
2016-03-13 13:03:12 +0000

    reversing

--- pt_web.hs
+++ pt_web.hs
@@ -8,6 +8,8 @@ import Data.Text
 import GHC.Generics
 import Data.Maybe
 import qualified Data.ByteString as S
+import qualified GHC.List as L
+import qualified Control.Monad as M
 
 pt_url = "https://www.pivotaltracker.com/services/v5/projects"
 pt_stories = pt_url ++ "/1367594/iterations?offset=" 
@@ -37,15 +39,22 @@ get_iterations s =
 get_all :: S.ByteString -> Int -> [[Iteration]] -> IO [[Iteration]]
 get_all tk offset its = do
 
-        s <- get_raw tk offset 
-        let i = fromJust $ get_iterations s 
+    s <- get_raw tk offset 
+    let i = fromJust $ get_iterations s 
 
-        putStrLn $ show offset
+    putStrLn $ show offset
 
-        if i == []
-          then return its 
-          else get_all tk (offset + 10) (i:its)
+    if i == []
+      then return its 
+      else get_all tk (offset + 10) (i:its)
 
 get_project :: S.ByteString -> IO [[Iteration]]
 get_project tk = get_all tk 0 []
+
+get_all_iterations tk = do 
+
+    its <- get_project tk 
+
+    let s = L.reverse its
+    return $ M.join s 
 
