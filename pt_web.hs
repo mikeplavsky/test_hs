a229d20ac37f9d4d80444766e9eacbcc67deed78
mikeplavsky <mike.plavsky@quest.com>
2016-03-13 12:40:06 +0000

    added types

--- pt_web.hs
+++ pt_web.hs
@@ -7,6 +7,7 @@ import Data.Aeson (Array, FromJSON, ToJSON, decode)
 import Data.Text
 import GHC.Generics
 import Data.Maybe
+import qualified Data.ByteString as S
 
 pt_url = "https://www.pivotaltracker.com/services/v5/projects"
 pt_stories = pt_url ++ "/1367594/iterations?offset=" 
@@ -33,6 +34,7 @@ get_iterations s =
     let d = s ^. responseBody
     in decode d :: Maybe [Iteration]
 
+get_all :: S.ByteString -> Int -> [[Iteration]] -> IO [[Iteration]]
 get_all tk offset its = do
 
         s <- get_raw tk offset 
@@ -43,4 +45,7 @@ get_all tk offset its = do
         if i == []
           then return its 
           else get_all tk (offset + 10) (i:its)
+
+get_project :: S.ByteString -> IO [[Iteration]]
+get_project tk = get_all tk 0 []
 
