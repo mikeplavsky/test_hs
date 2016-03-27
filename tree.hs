a4aba84fe8b6ad3af4b069545c5071c2eba44ac8
mikeplavsky <mikeplavsky@quest.com>
2016-03-27 15:09:28 +0300

    monad for number

--- tree.hs
+++ tree.hs
@@ -5,11 +5,28 @@ instance Functor Tree where
     fmap f (Leaf a) = Leaf (f a)   
     fmap f (Branch l r) = Branch (fmap f l) (fmap f r)  
     
+-- number part
+
+return'' x = \s -> (x,s)
+(>>>=>>>) x f = \s -> let (a,s') = x s in f a s'
+
+number' (Branch l r) = 
+    number l >>>=>>> \l' ->
+    number r >>>=>>> \r' ->
+    return'' (Branch l' r')
+
+number' (Leaf a) = tick >>>=>>> \s ->
+    return'' (Leaf s)
+
+tick s = (s, s + 1)
+
 number (Leaf a) s = (Leaf s, s + 1)
 number (Branch l r) s = 
     let (l',s') = number l s
         (r',s'') = number r s'
     in (Branch l' r', s'')    
+
+-- zip part
 
 return' x = Just x
 (>>=>>) x f = 
