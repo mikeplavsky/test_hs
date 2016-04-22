0bf7b4f9b57a1b4f8b8db5208b9b7b5dbcba4497
mikeplavsky <mikeplavsky@quest.com>
2016-04-22 10:48:33 +0300

    trying state monad - does not work though

--- tree.hs
+++ tree.hs
@@ -9,37 +9,34 @@ instance Functor Tree where
     
 -- number part
 
+newtype State' s a = State' {runState :: s -> (a,s)}
+
+-- return'' :: a -> State' s a
+return'' a = \s -> (a,s)
+
 type State s a = s -> (a,s)
 
-return'' :: a -> State s a
-return'' a = \s -> (a,s)
+return' :: a -> State s a
+return' a = \s -> (a,s)
 
 (>>>=>>>) :: State s a -> (a -> State s b) -> State s b
 (>>>=>>>) x f = \s -> let (a,s') = x s in f a s'
 
-number' (Branch l r) = 
+number (Branch l r) = 
     number l >>>=>>> \l' ->
     number r >>>=>>> \r' ->
-    return'' (Branch l' r')
+    return' (Branch l' r')
 
-number' (Leaf a) = tick >>>=>>> \s ->
-    return'' (Leaf s)
+number (Leaf a) = tick >>>=>>> \s ->
+    return' (Leaf s)
 
 tick s = (s, s + 1)
-
-number (Leaf a) s = (Leaf s, s + 1)
-number (Branch l r) s = 
-    let (l',s') = number l s
-        (r',s'') = number r s'
-    in (Branch l' r', s'')    
 
 -- zip part
 
 zipTree (Leaf a) (Leaf b) = return $ Leaf (a, b)
 
 zipTree (Branch l r) (Branch l' r') = do
-    l'' <- zipTree l l'
-    r'' <- zipTree r r'
-    return $ Branch l'' r''
+    liftM2 Branch (zipTree l l') (zipTree r r')
 
 zipTree _ _ = Nothing
