d09751763ca642f3da70efca231e5fdbf2ee67b4
mikeplavsky <mikeplavsky@quest.com>
2016-05-05 11:24:53 +0300

    doing fib in parallel

--- parMap.hs
+++ parMap.hs
@@ -1,4 +1,5 @@
 import Control.Parallel.Strategies
+import System.Environment
 
 parMap' f [] = return []
 
@@ -8,5 +9,23 @@ parMap' f (a:as) = do
     bs <- parMap' f as
 
     return (b:bs)
+
+fib = 1:1:[x+y | (x,y) <- zip fib (tail fib)] 
+
+main' f = do
+
+    file <- readFile f
+
+    let ds = map read $ lines file  :: [Int]
+    let w = runEval (parMap' ((!!) fib) ds) 
+
+    return w
+
+main = do
+
+    [f] <- getArgs   
+    r <- main' f
+
+    print r
 
 
